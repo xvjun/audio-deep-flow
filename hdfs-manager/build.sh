45cc7274ba75ed92047d8e88e2b6af1c870a894d
Xu Jun <xujun@4paradigm.com>
2020-03-02 20:12:16 +0800

    init 4

--- hdfs-manager/build.sh
+++ hdfs-manager/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mvn clean package
 docker build -t hdfs-manager:1.0.0 .
-docker tag hdfs-manager:1.0.0 registry.cn-hangzhou.aliyuncs.com/audio-deep-flow/hdfs-manger:1.0.0
-docker push registry.cn-hangzhou.aliyuncs.com/audio-deep-flow/hdfs-manger:1.0.0
+docker tag hdfs-manager:1.0.0 registry.cn-hangzhou.aliyuncs.com/audio-deep-flow/hdfs-manager:1.0.0
+docker push registry.cn-hangzhou.aliyuncs.com/audio-deep-flow/hdfs-manager:1.0.0
