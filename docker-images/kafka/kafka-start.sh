a05f4decda5736299b762d4b728722465b500ec7
Xu Jun <xujun@4paradigm.com>
2020-03-02 17:52:27 +0800

    init 4

--- docker-images/kafka/kafka-start.sh
+++ docker-images/kafka/kafka-start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 docker stop xujun-kafka
 docker rm xujun-kafka
-
+cur_dir=$1
 docker run  -d --name xujun-kafka \
 -p 9092:9092 \
 -e KAFKA_BROKER_ID=0 \
--e KAFKA_ZOOKEEPER_CONNECT=192.168.0.104:2181 \
--e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://192.168.0.104:9092 \
+-e KAFKA_ZOOKEEPER_CONNECT=${cur_dir}:2181 \
+-e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${cur_dir}:9092 \
 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -t wurstmeister/kafka:2.12-2.4.0
